7571f00662ed49c09abe28cd2a4a9984cda443f0
Dodji Maboudou-Tchao <dodjimabfr@gmail.com>
2026-03-06 14:48:10 -0500

    Update R03_NUM03 J.java
    compliant version

--- R03_NUM03 J.java
+++ R03_NUM03 J.java
@@ -3,7 +3,8 @@ import java.io.IOException;
 
 public class Example {
 
-    public static int getInteger(DataInputStream is) throws IOException {
-        return is.readInt();
+    public static long getInteger(DataInputStream is) throws IOException {
+        return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bit values
     }
+
 }

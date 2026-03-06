b9a625e99236dbe7c18aed5f776b77fd810559a2
Dodji Maboudou-Tchao <dodjimabfr@gmail.com>
2026-03-06 14:36:44 -0500

    Update R02_XP00_J.java
    compliant version

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -4,5 +4,8 @@ public void deleteFile() {
     File someFile = new File("someFileName.txt");
 
     // Do something with someFile
-    someFile.delete();  // return value ignored (non-compliant)
+
+    if (!someFile.delete()) {
+        // Handle failure to delete the file
+    }
 }
